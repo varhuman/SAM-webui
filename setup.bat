d5af48e5642e3229433e106afa35e4074b83a8ed
varhuman <770420614@qq.com>
2023-04-09 16:45:31 +0800

    change bat to install ts

--- setup.bat
+++ setup.bat
@@ -1,7 +1,23 @@
 @echo off
 
+echo Installing virtualenv
 pip install --user virtualenv
+
+echo Creating virtual environment
 python -m venv env
+
+echo Activating virtual environment
 call env\Scripts\activate.bat
+
+echo Installing Python dependencies
 pip install git+https://github.com/facebookresearch/segment-anything.git
 pip install opencv-python pycocotools matplotlib onnxruntime onnx gradio
+
+echo Installing Node.js dependencies
+npm install -g typescript
+npm install
+
+echo Compiling TypeScript to JavaScript
+tsc
+
+echo Setup complete
--- setup.sh
+++ setup.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+echo "Installing virtualenv"
 pip install --user virtualenv
-python3 -m venv env
+
+echo "Creating virtual environment"
+python -m venv env
+
+echo "Activating virtual environment"
 source env/bin/activate
+
+echo "Installing Python dependencies"
 pip install git+https://github.com/facebookresearch/segment-anything.git
 pip install opencv-python pycocotools matplotlib onnxruntime onnx gradio
+
+echo "Installing Node.js dependencies"
+npm install -g typescript
+npm install
+
+echo "Compiling TypeScript to JavaScript"
+tsc
+
+echo "Setup complete"
